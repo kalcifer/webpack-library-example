2d7bb194350184fd3edeff0678fe23c055f62b34
Aman Raj <56553835+dgnext@users.noreply.github.com>
2019-12-16 13:36:42 +0530

    Update webpack.config.babel.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,3 +1,24 @@
+const path = require('path');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
++ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+  module.exports = {
+    entry: {
+      app: './src/index.js',
+      print: './src/print.js',
+    },
+    plugins: [
++     new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Output Management',
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  };
+
 module.exports = {
   //...
   optimization: {
