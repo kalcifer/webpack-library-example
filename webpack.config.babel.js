596770d9ad9e702ff23df81398944ad91ec32601
Aman Raj <56553835+dgnext@users.noreply.github.com>
2019-12-16 11:07:22 +0530

    Update webpack.config.babel.js
    Yes I do

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,30 @@
-const path = require('path');
+module.exports = {
+  //...
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minRemainingSize: 0,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+      automaticNameDelimiter: '~',
+      automaticNameMaxLength: 30,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
+};const path = require('path');
 
 export default () => (
     {
